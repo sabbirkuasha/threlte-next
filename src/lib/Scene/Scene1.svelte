10dd283d47362521d05e3d883f2e719f7166e40b
Sabbirz <sabbir.kuasha@gmail.com>
2023-06-21 15:51:31 +0600

    3d Model Added

--- src/lib/Scene/Scene1.svelte
+++ src/lib/Scene/Scene1.svelte
@@ -2,6 +2,7 @@
 	import { T } from '@threlte/core';
 	import { OrbitControls, TransformControls } from '@threlte/extras';
 	import { BoxGeometry, MeshStandardMaterial } from 'three';
+	import Cloth from '../../3dComponent/Cloth.svelte';
 </script>
 
 <T.PerspectiveCamera makeDefault position={[10, 5, 10]} lookAt.y={0.0}>
@@ -12,9 +13,9 @@
 <T.AmbientLight intensity={0.3} />
 
 <T.GridHelper args={[10, 10]} />
-
-<T.Mesh
+<Cloth scale={5} />
+<!-- <T.Mesh
 	position={[0, 1, 0]}
 	geometry={new BoxGeometry(2, 2, 2)}
 	material={new MeshStandardMaterial()}
-/>
+/> -->
