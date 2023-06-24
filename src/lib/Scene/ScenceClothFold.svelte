481760ecb24273f9d25cd74ec748a6fee4851aa4
sabbirz <sabbir.kuasha@gmail.com>
2023-06-24 11:54:42 +0600

    cloth folded added

---
+++ src/lib/Scene/ScenceClothFold.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { T } from '@threlte/core';
+	import { Environment, OrbitControls, TransformControls } from '@threlte/extras';
+	import { BoxGeometry, DirectionalLightHelper, MeshStandardMaterial } from 'three';
+	import ClothFolded from '../../3dComponent/ClothFolded.svelte';
+</script>
+
+<Environment path="/hdr/" files="stadium_01_2k.hdr" />
+
+<T.PerspectiveCamera makeDefault position={[10, 5, 10]} lookAt.y={0.0}>
+	<OrbitControls enableDamping />
+</T.PerspectiveCamera>
+
+<T.GridHelper args={[10, 10]} />
+<ClothFolded scale={3} />
---
+++ src/routes/clothFolded/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Canvas } from '@threlte/core';
+	import ScenceClothFold from '../../lib/Scene/ScenceClothFold.svelte';
+</script>
+
+<main>
+	<section class="p-5">
+		<div class=" ring w-full h-[800px]">
+			<Canvas>
+				<ScenceClothFold />
+			</Canvas>
+		</div>
+	</section>
+</main>
